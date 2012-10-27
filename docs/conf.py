127af91980402d7ed7125d92417eb6f6024930b4
rwood-moz <rwood@mozilla.com>
2012-10-27 15:27:12 -0400

    Add sphinx-based documentation for mozb2g

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../mozfile'))
+sys.path.insert(0, os.path.abspath('../mozb2g'))
 
 # -- General configuration -----------------------------------------------------
 
--- docs/index.rst
+++ docs/index.rst
@@ -16,6 +16,7 @@ Contents:
    :maxdepth: 2
 
    overview
+   mozb2g
    mozfile
 
 Indices and tables
